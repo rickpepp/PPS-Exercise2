6fa454f760f339b07892698a97fe32ddf0d38060
Pepe Riccardo <riccardo.pepe3@studio.unibo.it>
2025-03-01 17:19:31 +0100

    Refactored magic number in SimpleBankAccountTest

--- basic-refactoring-exercise/src/test/java/SimpleBankAccountTest.java
+++ basic-refactoring-exercise/src/test/java/SimpleBankAccountTest.java
@@ -12,6 +12,10 @@ import static org.junit.jupiter.api.Assertions.*;
  */
 class SimpleBankAccountTest {
 
+    public static final int FIRST_DEPOSIT_AMOUNT = 100;
+    public static final int INVALID_WITHDRAW_AMOUNT = 120;
+    public static final int VALID_WITHDRAW_AMOUNT = 70;
+
     private AccountHolder accountHolder;
     private AccountHolder falseAccountHolder;
     private BankAccount bankAccount;
@@ -30,57 +34,59 @@ class SimpleBankAccountTest {
 
     @Test
     void testDeposit() throws InvalidIdException {
-        bankAccount.deposit(accountHolder, 100);
-        assertEquals(100, bankAccount.getBalance());
+        bankAccount.deposit(accountHolder, FIRST_DEPOSIT_AMOUNT);
+        assertEquals(FIRST_DEPOSIT_AMOUNT, bankAccount.getBalance());
     }
 
     @Test
     void testWrongIDDeposit() throws InvalidIdException {
-        bankAccount.deposit(accountHolder, 100);
+        bankAccount.deposit(accountHolder, FIRST_DEPOSIT_AMOUNT);
         assertThrows(InvalidIdException.class,
-                () -> bankAccount.deposit(falseAccountHolder, 50),
+                () -> bankAccount.deposit(falseAccountHolder, VALID_WITHDRAW_AMOUNT),
                 "Expected InvalidIdException because of different id");
     }
 
     @Test
     void testWithdraw() throws InvalidIdException, InsufficientBudgetException {
-        bankAccount.deposit(accountHolder, 100);
-        bankAccount.withdraw(accountHolder, 70);
-        assertEquals(30 - bankAccount.getWithdrawalFee(), bankAccount.getBalance());
+        bankAccount.deposit(accountHolder, FIRST_DEPOSIT_AMOUNT);
+        bankAccount.withdraw(accountHolder, VALID_WITHDRAW_AMOUNT);
+        assertEquals(FIRST_DEPOSIT_AMOUNT - VALID_WITHDRAW_AMOUNT - bankAccount.getWithdrawalFee(),
+                bankAccount.getBalance());
     }
 
     @Test
     void testWrongIDWithdraw() throws InvalidIdException {
-        bankAccount.deposit(accountHolder, 100);
+        bankAccount.deposit(accountHolder, FIRST_DEPOSIT_AMOUNT);
         assertThrows(InvalidIdException.class,
-                () -> bankAccount.withdraw(falseAccountHolder, 70),
+                () -> bankAccount.withdraw(falseAccountHolder, VALID_WITHDRAW_AMOUNT),
                 "Expected InvalidIdException because of different id");
     }
 
     @Test
     void testInsufficientBudgetWithdraw() throws InvalidIdException {
-        bankAccount.deposit(accountHolder, 100);
+        bankAccount.deposit(accountHolder, FIRST_DEPOSIT_AMOUNT);
         assertThrows(InsufficientBudgetException.class,
-                () -> bankAccount.withdraw(accountHolder, 120),
+                () -> bankAccount.withdraw(accountHolder, INVALID_WITHDRAW_AMOUNT),
                 "Asked 120 with budget 100, expected InsufficientBudgetException");
     }
 
     @Test
     void testInsufficientBudgetWithdrawWithFEE() throws InvalidIdException {
-        bankAccount.deposit(accountHolder, 100);
+        bankAccount.deposit(accountHolder, FIRST_DEPOSIT_AMOUNT);
         assertThrows(InsufficientBudgetException.class,
-                () -> bankAccount.withdraw(accountHolder, 100 + (bankAccount.getWithdrawalFee() == 0 ? 1.1 : bankAccount.getWithdrawalFee())),
+                () -> bankAccount.withdraw(accountHolder,
+                        FIRST_DEPOSIT_AMOUNT + (bankAccount.getWithdrawalFee() == 0 ? 1 : bankAccount.getWithdrawalFee())),
                 "Asked 100 with budget 100, expected InsufficientBudgetException because of FEE");
     }
 
     @Test
     void testInsufficientBudgetWithdrawHandled() throws InvalidIdException {
-        bankAccount.deposit(accountHolder, 100);
+        bankAccount.deposit(accountHolder, FIRST_DEPOSIT_AMOUNT);
         try {
-            bankAccount.withdraw(accountHolder, 120);
+            bankAccount.withdraw(accountHolder, INVALID_WITHDRAW_AMOUNT);
         } catch (InsufficientBudgetException e) {
-            assertEquals(100, bankAccount.getBalance());
+            assertEquals(FIRST_DEPOSIT_AMOUNT, bankAccount.getBalance());
         }
-        assertEquals(100, bankAccount.getBalance());
+        assertEquals(FIRST_DEPOSIT_AMOUNT, bankAccount.getBalance());
     }
 }
